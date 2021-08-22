601da1ae7001f5df3dd06d20f1e3d9a8856ff6ed
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2021-08-23 01:18:51 +0530

    Update TwoSum.cpp

--- Array/Easy/TwoSum.cpp
+++ Array/Easy/TwoSum.cpp
@@ -1,5 +1,6 @@
 //problem description
 /* 
+1. Two Sum
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
@@ -76,3 +77,23 @@ int main()
         cout<<res[i]<<" ";
     }
 }
+
+
+//167. Two Sum II - Input array is sorted
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+         vector<int>v1(2);
+        int i=0;
+        int j=numbers.size()-1;
+        while(i<j){
+            if(numbers[i] + numbers[j] == target){
+                v1[0]=i+1;
+                v1[1]=j+1; break;
+            }
+            else if(numbers[i] + numbers[j] > target)  j--;
+            else i++;
+        }
+        return v1;
+    }
+};
