be64fce3c126c56f09723db72e5c5e815d217b8e
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-07-15 09:52:31 +0530

    Weekly contest301

---
+++ LeetCode contests/contest301_Minimum_amt_time_fill_cups.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int fillCups(int[] amount) {
+        if(amount[0] == amount[1] && amount[1]==amount[2]){
+            int sum = amount[0]*3;
+            if(sum %2 ==0) return sum/2;
+            else return sum/2+1;
+        }
+        
+        int maxi = Math.max(amount[0], amount[1]);
+        maxi = Math.max(maxi, amount[2]);
+        int time=0;
+        
+        int sumt = amount[0]+amount[1]+amount[2];
+        int lhs = sumt-maxi;
+        if(maxi>=lhs) return maxi;
+        else{
+            // Arrays.sort(array, Collections.reverseOrder());
+            Arrays.sort(amount);
+            // for(int i=0;i<3;i++) System.out.print(amount[i]+" ");
+            // System.out.println();
+            while(amount[0]>0 && amount[1]>0 && amount[2]>0){
+                if(amount[1]>0 && amount[2]>0){
+                    time++;  amount[1]--; amount[2]--;
+                    // System.out.println(amount[0]+" "+amount[1]+" "+amount[2]+" "+time);
+                    Arrays.sort(amount);
+                }
+                else break;
+            }
+            time += amount[2];
+            return time;
+        }
+        
+        
+        // return -1;
+        // if(amount[0] == 0 && amount[1] == 0 && amount[2] == 0)   return 0;
+        // int time=0;
+        // while(amount[0] != 0 || amount[1] != 0 || amount[2] != 0){
+        //     if(amount[0] != 0  && amount[1] != 0){
+        //         amount[0]--;   amount[1]--;    time++;
+        //     }
+        //     if(amount[1] != 0  && amount[2] != 0){
+        //         amount[1]--;   amount[2]--;    time++;
+        //     }
+        //     if(amount[0] != 0  && amount[2] != 0){
+        //         amount[0]--;   amount[2]--;    time++;
+        //     }
+        //     if(amount[0] != 0  &&(amount[1]==0 && amount[2]==0)){
+        //         time += amount[0];   amount[0]=0;
+        //     }
+        //     if(amount[1] != 0  &&(amount[0]==0 && amount[2]==0)){
+        //         time += amount[1];   amount[1]=0;
+        //     }
+        //     if(amount[2] != 0  &&(amount[1]==0 && amount[0]==0)){
+        //         time += amount[2];   amount[2]=0;
+        //     }
+        // }
+        // return time;
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # codingStreak
 
-
 ## Leetcode Daily Challenge
 
 - [Construct Binary Tree from Preorder and Inorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/) - Leetcode medium(14th July 2022)
 
 - [Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/) - Leetcode medium(13th July 2022)
 
+
+
+## Leetcode Weekly Contests
+
+###  [Weekly Contest 301](https://leetcode.com/contest/weekly-contest-301/)
+
+- [Minimum Amount of Time to Fill Cups](https://leetcode.com/contest/weekly-contest-301/problems/minimum-amount-of-time-to-fill-cups/)
