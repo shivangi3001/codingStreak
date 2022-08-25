0d86b5c05fd392b0ba6afb708c48642a41e15141
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-08-25 23:01:40 +0530

    optimised approach

--- 136_Single_Number.java
+++ 136_Single_Number.java
@@ -1,23 +1,28 @@
+
+// bit manipulation approach -- 2ms
 class Solution {
     public int singleNumber(int[] nums) {
-//         int a = nums[0];
-//         int len = nums.length;
-        
-//         for(int i=1;i<len;i++){
-//             a = a^nums[i];
-//             System.out.println(a);
-//         }
-//         return -1;
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int result = 0;
         for(int i : nums){
-            if(hm.containsKey(i))  hm.put(i, hm.get(i)+1);
-            else   hm.put(i, 1);
+            result = result^i;
         }
-        
-        int val =0;
-        for(int i : hm.keySet()){
-            if(hm.get(i) == 1)  val =i;
-        }
-        return val;
+        return result;
     }
 }
+
+// hashmap approach --- 12ms
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashMap<Integer, Integer> hm = new HashMap<>();
+//         for(int i : nums){
+//             if(hm.containsKey(i))  hm.put(i, hm.get(i)+1);
+//             else   hm.put(i, 1);
+//         }
+        
+//         int val =0;
+//         for(int i : hm.keySet()){
+//             if(hm.get(i) == 1)  val =i;
+//         }
+//         return val;
+//     }
+// }
