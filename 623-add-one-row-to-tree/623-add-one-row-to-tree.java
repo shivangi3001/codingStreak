6a4e519bb1d4466eca6e0d9ff9a1ab2926145dac
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-10-06 12:15:20 +0530

    Time: 1 ms (88.29%), Space: 46 MB (14.43%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -13,49 +13,71 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode addOneRow(TreeNode root, int val, int depth) {
-//         if(root == null)  return root;
-        
-//     }
-// }
+// my approach
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth == 1){
+            TreeNode n1 = new TreeNode(val);
+            n1.left = root;
+            return n1;
+        }
+        helper(root, val, depth, 1);
+        return root;
+    }
+    
+    public void helper(TreeNode root, int val, int depth, int d){
+        if(root == null)  return;
+        if(d==depth-1){
+            TreeNode t1 = root.left;
+            root.left = new TreeNode(val);
+            root.left.left = t1;
+            TreeNode t2 = root.right;
+            root.right = new TreeNode(val);
+            root.right.right = t2;
+        }
+        else{
+            helper(root.left, val, depth, d+1);
+            helper(root.right, val, depth, d+1);
+        }
+    }
+}
 
 
 // discussion vala approach
-class Solution {
-    public TreeNode addOneRow(TreeNode root, int val, int depth) {
-        return helper(root,val,depth,0);
+// class Solution {
+//     public TreeNode addOneRow(TreeNode root, int val, int depth) {
+//         return helper(root,val,depth,0);
         
         
-    }
-    public TreeNode helper(TreeNode root, int val, int depth,int flag){
-        if(depth==1 && flag==0){
-            TreeNode newNode=new TreeNode(val);
-            newNode.left=root;
-            return newNode;
-        }
-        if(depth==1 && flag==1){
-            TreeNode newNode=new TreeNode(val);
-            newNode.right=root;
-            return newNode;
-        }
+//     }
+//     public TreeNode helper(TreeNode root, int val, int depth,int flag){
+//         if(depth==1 && flag==0){
+//             TreeNode newNode=new TreeNode(val);
+//             newNode.left=root;
+//             return newNode;
+//         }
+//         if(depth==1 && flag==1){
+//             TreeNode newNode=new TreeNode(val);
+//             newNode.right=root;
+//             return newNode;
+//         }
         
-        if(root.left!=null){
-            root.left=helper(root.left,val,depth-1,0);
-        }
-        else if(root.left==null && depth==2){
-            root.left=new TreeNode(val);
-        }
+//         if(root.left!=null){
+//             root.left=helper(root.left,val,depth-1,0);
+//         }
+//         else if(root.left==null && depth==2){
+//             root.left=new TreeNode(val);
+//         }
         
-        if(root.right!=null){
-            root.right=helper(root.right,val,depth-1,1);
-        }
-        else if(root.right==null && depth==2){
-            root.right=new TreeNode(val);
-        }
-        return root;
+//         if(root.right!=null){
+//             root.right=helper(root.right,val,depth-1,1);
+//         }
+//         else if(root.right==null && depth==2){
+//             root.right=new TreeNode(val);
+//         }
+//         return root;
         
-    }
+//     }
     
     
-}
+// }
