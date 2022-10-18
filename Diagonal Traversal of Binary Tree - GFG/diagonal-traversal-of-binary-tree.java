907dc669d77974080a9310e02f07c74e0a5e6be3
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-10-18 09:07:15 +0530

    Added solution - LeetHub

---
+++ Diagonal Traversal of Binary Tree - GFG/diagonal-traversal-of-binary-tree.java
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Tree g = new Tree();
+			    ArrayList<Integer> diagonalNode = g.diagonal(root);
+			    for(int i = 0 ;i<diagonalNode.size();i++){
+			        System.out.print(diagonalNode.get(i)+ " ");
+			    }
+		        System.out.println();
+	        }
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Node is defined as
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}*/
+class Tree
+{
+     public ArrayList<Integer> diagonal(Node root)
+      {
+           //add your code here.
+           ArrayList<Integer> list = new ArrayList<>();
+           if(root == null) return list;
+           
+           Queue<Node> q1 = new ArrayDeque<>();
+           q1.add(root);   //list.add(root.data);
+           
+           while(q1.size()>0){
+               
+               Node n1 = q1.poll();
+               list.add(n1.data);
+               while(n1 != null){
+                   if(n1.left != null) q1.add(n1.left);
+                   if(n1.right != null){
+                       n1= n1.right;  list.add(n1.data);
+                   }
+                   else n1=n1.right;
+               }
+            //   if(root.left != null) q1.add(root.left);
+            //   if(root.right == null){
+            //       root = q1.poll();  list.add(root.data);
+            //   }
+            //   if(root.right != null) l1.add(root.right.data);
+            //   root=root.right;
+           }
+           return list;
+      }
+}
