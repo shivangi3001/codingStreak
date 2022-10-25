8995fd4fbdbe17fdc7ca02e5b10e000e581eb589
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2022-10-25 15:52:02 +0530

    Time: 1 ms (95.64%), Space: 42.1 MB (48.24%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -7,10 +7,14 @@ class Solution {
         
         for(String i : word1) s1.append(i);
         for(String i : word2) s2.append(i);
-        String str1=s1.toString();    String str2=s2.toString();
+        //String str1=s1.toString();    String str2=s2.toString();
         
         //System.out.println(str1+"->"+str2);
-        if(str1.equals(str2)) return true;
-        return false;
+        // if(str1.equals(str2)) return true;
+        // return false;
+        return s1.toString().equals(s2.toString());
     }
 }
+
+// removing variable boolean flag -- then --> Runtime: 1 ms, faster than 95.64%
+//     Memory Usage: 41.6 MB, less than 78.48% 
