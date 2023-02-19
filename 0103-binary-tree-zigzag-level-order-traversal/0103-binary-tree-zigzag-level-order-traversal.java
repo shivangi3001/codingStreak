caac0c62b025eabc2d788365a77b9b0a5e41d7df
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2023-02-19 13:56:56 +0530

    Time: 1 ms (87.53%), Space: 41 MB (69.74%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// daily leetcode challenge - 19th Feb 2023
+
+// testcases -- [3,9,20,null,null,15,7]
+// [1]
+// []
+
+// Runtime: 1 ms, faster than 96.90%  Memory Usage: 40.8 MB, less than 96.98% 
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        if(root == null)   return l1;
+        else if(root.left == null && root.right == null){
+            l2.add(root.val);  l1.add(l2);   return l1;
+        }
+        
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        st1.push(root);   int level=0;
+        
+        while(st1.size()>0){
+            TreeNode n1 = st1.pop();
+            l2.add(n1.val);
+            if(level%2 != 0){
+                if(n1.right != null)  st2.push(n1.right);
+                if(n1.left != null)  st2.push(n1.left);
+                
+            }
+            else{
+                if(n1.left != null)  st2.push(n1.left);
+                if(n1.right != null)  st2.push(n1.right);
+            }
+            
+            if(st1.size()==0){
+                st1=st2;   st2=new Stack<>(); level++;
+                l1.add(new ArrayList<>(l2));
+                l2=new ArrayList<>();
+            }
+        }
+        
+        return l1;
+    }
+}
